0d2801fc0db3c63e77493c7c2a84ba7ade0fa9cc
Lucas-Qin <niki.grey.uk@gmail.com>
2022-09-28 23:39:09 +1000

    errorMiddleware.js added, async http request added

--- server/controller/composerController.js
+++ server/controller/composerController.js
@@ -1,34 +1,39 @@
+const asyncHandler = require('express-async-handler')
 //@desc get composer
 //@route GET api/composer
 //@access Private
 
-const getComposer = (req, res)=>{
+const getComposer = asyncHandler(async (req, res)=>{
     res.status(200).json({message:'get composer'})
-}
+})
 
 //@desc set composer
 //@route POST api/composer
 //@access Private
 
-const setComposer = (req, res)=>{
+const setComposer = asyncHandler(async (req, res)=>{
+    if(!req.body.text){
+        res.status(400)
+        throw new Error("Empty input")
+    }
     res.status(200).json({message:'set composer'})
-}
+})
 
 //@desc Update composer
 //@route Put api/composer/:id
 //@access Private
 
-const updateComposer = (req, res)=>{
+const updateComposer = asyncHandler(async (req, res)=>{
     res.status(200).json({message:'update composer'})
-}
+})
 
 //@desc delete composer
 //@route DELETE api/composer/:id
 //@access Private
 
-const deleteComposer = (req, res)=>{
+const deleteComposer = asyncHandler(async (req, res)=>{
     res.status(200).json({message:'delete composer'})
-}
+})
 
 module.exports = {
     getComposer,
