3f7d3aa188a64d1e222067cccee9c6a7f97b3c91
Yichen Song <songdaxian123@gmail.com>
2022-09-30 13:52:06 +1000

    create server

---
+++ server/models/user.js
@@ -0,0 +1,30 @@
+const Joi = require('joi');
+const mongoose = require('mongoose');
+
+const User = mongoose.model('User', new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 255,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 1024,
+    }
+}));
+
+function validateUser(user) {
+    const schema = {
+        email: Joi.string().min(5).max(50).required().email(),
+        password: Joi.string().min(5).max(50).required(),
+    };
+
+    return Joi.validate(user, schema);
+}
+
+exports.User = User;
+exports.validate = validateUser;
