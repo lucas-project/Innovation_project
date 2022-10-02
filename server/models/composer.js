30ce50496e3e33756513df0fc61f4ff3e417a88b
maye244 <104114257+maye244@users.noreply.github.com>
2022-10-02 20:06:43 +1100

    Change the composer schema

--- server/models/composer.js
+++ server/models/composer.js
@@ -8,12 +8,7 @@ const Composer = mongoose.model('Composer', new mongoose.Schema({
         minlength: 2,
         maxlength: 255,
     },
-    born: {
-        type: Number,
-        minlength: 1,
-        maxlength: 50,
-    },
-    death: {
+    date: {
         type: Number,
         minlength: 1,
         maxlength: 50,
@@ -28,16 +23,21 @@ const Composer = mongoose.model('Composer', new mongoose.Schema({
         type: String,
         minlength: 2,
         maxlength: 255,
-    }
+    },
+    biography: {
+        type: String,
+        minlength: 2,
+        maxlength: 1024,
+    },
 }));
 
 function validateComposer(composer) {
     const schema = Joi.object({
         name: Joi.string().min(2).max(255).required(),
-        born: Joi.number().min(1).max(50),
-        death: Joi.number().min(1).max(50),
+        date: Joi.number().min(1).max(50),
         nationality: Joi.string().min(2).max(50).required(),
         website: Joi.string().min(2).max(255),
+        biography: Joi.string().min(2).max(1024),
     });
 
     return schema.validate(composer);
