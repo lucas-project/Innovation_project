fa3e3139bb5a2d6eb29c3ec972af44d7171ec336
maye244 <104114257+maye244@users.noreply.github.com>
2022-10-02 18:45:26 +1100

    Change the composer schema

--- server/models/composer.js
+++ server/models/composer.js
@@ -8,9 +8,14 @@ const Composer = mongoose.model('Composer', new mongoose.Schema({
         minlength: 2,
         maxlength: 255,
     },
-    date: {
-        type: String,
-        required: true,
+    born: {
+        type: Number,
+        //required: true,
+        minlength: 1,
+        maxlength: 50,
+    },
+    death: {
+        type: Number,
         minlength: 1,
         maxlength: 50,
     },
@@ -30,7 +35,8 @@ const Composer = mongoose.model('Composer', new mongoose.Schema({
 function validateComposer(composer) {
     const schema = Joi.object({
         name: Joi.string().min(2).max(255).required(),
-        date: Joi.string().min(1).max(50).required(),
+        born: Joi.number().min(1).max(50),
+        death: Joi.number().min(1).max(50),
         nationality: Joi.string().min(2).max(50).required(),
         website: Joi.string().min(2).max(255),
     });
--- server/routes/composers.js
+++ server/routes/composers.js
@@ -14,7 +14,7 @@ router.post('/', [auth, admin], async (req, res) => {
     const { error } = validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
-    const composer = new Composer(_.pick(req.body, ['name', 'date', 'nationality', 'website']));
+    const composer = new Composer(_.pick(req.body, ['name', 'born','death', 'nationality', 'website']));
     await composer.save();
 
     res.send(composer);
@@ -24,7 +24,7 @@ router.put('/:id', [auth, admin], async (req, res) => {
     const { error } = validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
-    const composer = await Composer.findByIdAndUpdate(req.params.id, _.pick(req.body, ['name', 'date', 'nationality', 'website']), { new: true });
+    const composer = await Composer.findByIdAndUpdate(req.params.id, _.pick(req.body, ['name', 'born','death', 'nationality', 'website']), { new: true });
 
     if (!composer) return res.status(404).send('The composer with the given ID was not found.');
 
