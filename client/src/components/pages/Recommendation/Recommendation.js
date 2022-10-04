6a05c56d58603e3a68b9acee22b92665c544a571
NatalieInTheVibe <nongtianh6@gmail.com>
2022-10-05 00:09:16 +1100

    Update recommendation page and Home navigation

--- client/src/components/pages/Recommendation/Recommendation.js
+++ client/src/components/pages/Recommendation/Recommendation.js
@@ -4,6 +4,9 @@ import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Ailis from '../../../img/ailis.jpg';
+import Nicola from '../../../img/Nicola.jpg';
+import "./Recommendation.css";
 
 const Recommendation = () => {
     return (
@@ -12,12 +15,11 @@ const Recommendation = () => {
         <div class="col col-4 d-flex justify-content-center">
             {/* <div style={{ position: "absolute", margin: "170px", fontSize: "20px" }}> */}
                 <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src="../../img/ailis.jpg" alt="ailis"/>
+                <Card.Img variant="top" src={Ailis} alt="ailis"/>
                 <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
+                    <Card.Title>Ailís Ní Ríain</Card.Title>
                     <Card.Text>
-                    Some quick example text to build on the card title and make up the
-                    bulk of the card's content.
+                     She is a contemporary classical composer and published writer for stage who aims to produce work that challenges, provokes and engages.
                     </Card.Text>
                     <Button variant="primary">View more details.</Button>
                 </Card.Body>
@@ -27,12 +29,11 @@ const Recommendation = () => {
         <div class="col col-4 d-flex justify-content-center">
         {/* <div style={{ position: "absolute", margin: "170px", fontSize: "20px" }}> */}
                 <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Img variant="top" src={Nicola} alt="Nicola" />
                 <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
+                    <Card.Title>Nicola LeFanu</Card.Title>
                     <Card.Text>
-                    Some quick example text to build on the card title and make up the
-                    bulk of the card's content.
+                    Her catalogue includes a number of works for string ensemble, and chamber music for a wide variety of mediums, often including voice. She has a particular affinity for vocal music and has composed eight operas.
                     </Card.Text>
                     <Button variant="primary">View more details.</Button>
                 </Card.Body>
@@ -42,7 +43,7 @@ const Recommendation = () => {
         <div class="col col-4 d-flex justify-content-center">
         {/* <div style={{ position: "absolute", margin: "170px", fontSize: "20px" }}> */}
                 <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Img variant="top"  src="holder.js/100px180" />
                 <Card.Body>
                     <Card.Title>Card Title</Card.Title>
                     <Card.Text>
