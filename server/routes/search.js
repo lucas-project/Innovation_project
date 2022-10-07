5d04eaef88c718e919f064397b2eaedb37309ba2
Yichen Song <songdaxian123@gmail.com>
2022-10-07 16:01:35 +1100

    -update search

--- server/routes/search.js
+++ server/routes/search.js
@@ -28,4 +28,35 @@ router.get('/:key', async (req, res) => {
     res.send(result);
 });
 
+router.get('/composer/:key', async (req, res) => {
+
+    const reg = new RegExp(req.params.key, "i");
+    let result = await Composer.find(
+        {
+            "$or": [
+                { name: { $regex: reg } },
+                { nationality: { $regex: reg } }
+            ]
+        }
+    );
+
+    res.send(result);
+});
+
+router.get('/piece/:key', async (req, res) => {
+
+    const reg = new RegExp(req.params.key, "i");
+
+    let result = await Piece.find(
+        {
+            "$or": [
+                { name: { $regex: reg } },
+                { instruments: { $regex: reg } }
+            ]
+        }
+    );
+
+    res.send(result);
+});
+
 module.exports = router; 
