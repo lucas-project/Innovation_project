2916a41113b28edc1a06c59154e0e91488f348cb
NatalieInTheVibe <nongtianh6@gmail.com>
2022-10-10 10:39:44 +1100

    update draft for axios

--- client/src/components/pages/Search/Search.js
+++ client/src/components/pages/Search/Search.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from "styled-components";
 // import Dropdown from 'react-bootstrap/Dropdown';
 // import DropdownButton from 'react-bootstrap/DropdownButton';
@@ -7,13 +7,14 @@ import { Link } from "react-router-dom";
 import Form from 'react-bootstrap/Form';
 import { useNavigate} from "react-router-dom";
 import axios from 'axios';
-
+import Card from 'react-bootstrap/Card';
 
 
 const Search = () => {
     const navigate = useNavigate();
     const [searchArray, setSearchArray] = useState(["","","","Any"]);
     const [URL,setURL] = useState([""]);
+    const [info,setInfo] = useState(["","",""]);
     // const [filterInput1, setFilterInput1] = useState('');
     // const [filterInput2, setFilterInput2] = useState('');
     // const [searchBy, setSearchBy] = useState('Any');
@@ -54,7 +55,23 @@ const Search = () => {
         setURL(URL);
         console.log(newValue,searchArray,URL);
         navigate(URL);
+        //componentDidMount(URL);
     };
+    
+    // useEffect(() =>{
+    //     axios
+    //     .get('http://localhost:3000/Search/'+URL)
+    //     .then(res =>{
+    //         setInfo(res.data);
+    //             // res.data[1],
+    //             // res.data[2]
+
+    //     })
+    //     .catch(err =>{
+    //         console.log("Err from get search result")
+    //     })
+    // },[]);
+
     // const filterItems1 = (filterValue1) => {
     //     setFilterInput1(filterValue1);
     //     console.log(filterValue1); 
@@ -84,92 +101,104 @@ const Search = () => {
     // const [query, searchItems] = useState("");
     return (
     <StyledSearch>
-        <div class="container mt-2">
-          <div class="row d-flex justify-content-center">
-            <div class="col-md-12">
-              <div class="card p-3  py-4">
-                <h5>Search to explore the greatest female composers</h5>
-                <div class="row g-3 mt-2">
-                    <div class="col-md-3 dropdown">
-                        {/* <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Dropdown
-                        </button> */}
-        {/* id="dropdown-basic-button" title="Search By Any"  */}
-                        {/* <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
-                            <button class="dropdown-item" type="button" value={searchBy} onClick={() => setSearchBy("Name")}>By Name</button>
-                            <button class="dropdown-item" type="button" value={searchBy} onClick={() => setSearchBy("Name")}>By Piece</button>
-                            <button class="dropdown-item" type="button" value={searchBy} onClick={() => setSearchBy("Name")}>By Year of Piece</button>
-                        </div> */}
-                        {/* <DropdownButton id="dropdown-item-button" title="Search By Any">
-                            <Dropdown.Item as="button" value={searchBy} onClick={() => setSearchBy("Name")}>By Name</Dropdown.Item>
-                            <Dropdown.Item value={searchBy} onClick={() => setSearchBy("Piece")}>By Piece</Dropdown.Item>
-                            <Dropdown.Item value={searchBy} onClick={() => setSearchBy("Year")}>By Year of Piece</Dropdown.Item>
-                        </DropdownButton>
-                        <DropdownButton id="dropdown-item-button" title="Search By Any">
-                            <Dropdown.Item  value={searchBy} onSelect={() => setSearchBy("Name")}>By Name</Dropdown.Item>
-                            <Dropdown.Item >By Piece</Dropdown.Item>
-                            <Dropdown.Item href="#/a">By Year of Pieceee</Dropdown.Item>
-                        </DropdownButton> */}
-                        <Form.Select aria-label="Default select example">
-                            <option>Search By Any</option>
-                            <option onSelect={() => searchItems("Name","searchBy")}>Search By Name</option>
-                            <option onSelect={() => searchItems("Piece","searchBy")}>Search By Piece</option>
-                            <option onSelect={() => searchItems("Year","searchBy")}>Search By Year Of Piece</option>
-                        </Form.Select>
-                    </div>
-                    
-
-        {/* <div>
-            <Dropdown >
-                <Dropdown.Toggle variant="success" id="dropdown-basic">
-                    {selectedItem?selectedItem:"Select a country"}
-                </Dropdown.Toggle>
-                <Dropdown.Menu>
-                    <Dropdown.Item onSelect={()=>setSelectedItem("aaa")}>aaa</Dropdown.Item>
-                </Dropdown.Menu>
-            </Dropdown>
-        </div> */}
-                    
-
-                    
-                    <div class="col-md-6">
-                  {/* // onChange={() => setSearchInput(searchInput)}  */}
-                  {/* onChange={(e) => searchItems(e.target.value)}  */}
-                      <input type="text" class="form-control" onChange={(e) => searchItems(e.target.value,"searchInput")} placeholder="Enter keywords..."/>
-                  </div>
-                  <div class="col-md-3">
-                    <button class="btn btn-secondary btn-block"  onClick={onClickSearchHandler} type='submit'>Search Results</button>
-                  </div>   
-                </div>
-                <div class="mt-3">      
-                  <a
-                      onClick={() => setOpen(!open)}
-                      aria-controls="advancedSearch"
-                      aria-expanded={open}
-                    >
-                  Advanced Search with Filters
-                  <i class="fa fa-angle-down"></i></a> 
-                  <Collapse in={open}>
-                    <div id="advancedSearch">
-                      <div class="card card-body">
-                        <div class="row">
-                          <div class="col-md-6">
-                            <input type="text" placeholder="Nationality" class="form-control" onChange={(e) => searchItems(e.target.value,"filterInput1")}/>
-                          </div>
-                          <div class="col-md-6">
-                            <input type="text" class="form-control" placeholder="Instrument" onChange={(e) => searchItems(e.target.value,"filterInput2")}/>
-                          </div>    
+            <div class="container mt-2">
+                <div class="row d-flex justify-content-center">
+                    <div class="col-md-12">
+                        <div class="card p-3  py-4">
+                            <h5>Search to explore the greatest female composers</h5>
+                            <div class="row g-3 mt-2">
+                                <div class="col-md-3 dropdown">
+                                    {/* <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        Dropdown
+    </button> */}
+                                    {/* id="dropdown-basic-button" title="Search By Any"  */}
+                                    {/* <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
+        <button class="dropdown-item" type="button" value={searchBy} onClick={() => setSearchBy("Name")}>By Name</button>
+        <button class="dropdown-item" type="button" value={searchBy} onClick={() => setSearchBy("Name")}>By Piece</button>
+        <button class="dropdown-item" type="button" value={searchBy} onClick={() => setSearchBy("Name")}>By Year of Piece</button>
+    </div> */}
+                                    {/* <DropdownButton id="dropdown-item-button" title="Search By Any">
+        <Dropdown.Item as="button" value={searchBy} onClick={() => setSearchBy("Name")}>By Name</Dropdown.Item>
+        <Dropdown.Item value={searchBy} onClick={() => setSearchBy("Piece")}>By Piece</Dropdown.Item>
+        <Dropdown.Item value={searchBy} onClick={() => setSearchBy("Year")}>By Year of Piece</Dropdown.Item>
+    </DropdownButton>
+    <DropdownButton id="dropdown-item-button" title="Search By Any">
+        <Dropdown.Item  value={searchBy} onSelect={() => setSearchBy("Name")}>By Name</Dropdown.Item>
+        <Dropdown.Item >By Piece</Dropdown.Item>
+        <Dropdown.Item href="#/a">By Year of Pieceee</Dropdown.Item>
+    </DropdownButton> */}
+                                    <Form.Select aria-label="Default select example">
+                                        <option>Search By Any</option>
+                                        <option onSelect={() => searchItems("Name", "searchBy")}>Search By Name</option>
+                                        <option onSelect={() => searchItems("Piece", "searchBy")}>Search By Piece</option>
+                                        <option onSelect={() => searchItems("Year", "searchBy")}>Search By Year Of Piece</option>
+                                    </Form.Select>
+                                </div>
+                                {/* <div>
+        <Dropdown >
+            <Dropdown.Toggle variant="success" id="dropdown-basic">
+                {selectedItem?selectedItem:"Select a country"}
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+                <Dropdown.Item onSelect={()=>setSelectedItem("aaa")}>aaa</Dropdown.Item>
+            </Dropdown.Menu>
+        </Dropdown>
+    </div> */}
+                                <div class="col-md-6">
+                                    {/* // onChange={() => setSearchInput(searchInput)}  */}
+                                    {/* onChange={(e) => searchItems(e.target.value)}  */}
+                                    <input type="text" class="form-control" onChange={(e) => searchItems(e.target.value, "searchInput")} placeholder="Enter keywords..." />
+                                </div>
+                                <div class="col-md-3">
+                                    <button class="btn btn-secondary btn-block" onClick={onClickSearchHandler} type='submit'>Search Results</button>
+                                </div>
+                            </div>
+                            <div class="mt-3">
+                                <a
+                                    onClick={() => setOpen(!open)}
+                                    aria-controls="advancedSearch"
+                                    aria-expanded={open}
+                                >
+                                    Advanced Search with Filters
+                                    <i class="fa fa-angle-down"></i></a>
+                                <Collapse in={open}>
+                                    <div id="advancedSearch">
+                                        <div class="card card-body">
+                                            <div class="row">
+                                                <div class="col-md-6">
+                                                    <input type="text" placeholder="Nationality" class="form-control" onChange={(e) => searchItems(e.target.value, "filterInput1")} />
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <input type="text" class="form-control" placeholder="Instrument" onChange={(e) => searchItems(e.target.value, "filterInput2")} />
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </Collapse>
+                            </div>
                         </div>
-                      </div>
                     </div>
-                  </Collapse>
                 </div>
-              </div>
-            </div>     
-          </div>
-        </div>
-    </StyledSearch>
-  
+            </div>
+        </StyledSearch>
+         //display search result */}
+            // {/* <div>
+            //     <Card.Group itemsPerRow={3} style={{ marginTop: 20 }}>
+            //         {info.map((item) => {
+            //             return (
+            //                 <Card>
+            //                     <Card.Content>
+            //                         <Card.Header>{item.name}</Card.Header>
+            //                         <Card.Description>
+            //                             {item.email}
+            //                         </Card.Description>
+            //                     </Card.Content>
+            //                 </Card>
+            //             );
+            //         })}
+            //     </Card.Group>
+
+            // </div></> */}
     );
 };
 
