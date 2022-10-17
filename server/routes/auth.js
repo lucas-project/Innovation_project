d5c4ac9fa8cf117055065987b14dd3642574f3a2
maye244 <104114257+maye244@users.noreply.github.com>
2022-10-17 23:32:34 +1100

    add return information and modify format on login function

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,18 +4,33 @@ const express = require("express");
 const router = express.Router();
 
 router.post('/', async (req, res) => {
+    let result ="Success";
+    let token = "";
+    const email = req.body.email;
     const { error } = validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
     let user = await User.findOne({ email: req.body.email });
-    if (!user) return res.status(400).send('Invalid email or password.');
+    //if (!user) return res.status(400).send('Invalid email or password.');
+    if (!user) {
+        result = "Invalid email or password";
+        const sendback = {token,result};
+        return res.status(400).send(sendback);
+    }
+
 
     const validPassword = await bcrypt.compare(req.body.password, user.password);
-    if (!validPassword) return res.status(400).send('Invalid email or password.');
+    //if (!validPassword) return res.status(400).send('Invalid email or password.');
+    if (!validPassword) {
+        result = "Invalid email or password";
+        const sendback = {token,result};
+        return res.status(400).send(sendback);
+    }
 
-    const token = user.generateAuthToken();
+    token = user.generateAuthToken();
+    const sendback = {token,result,email};
 
-    res.send(token);
+    res.send(sendback);
 })
 
 
